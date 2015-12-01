84b848eccebde7086c5b648019270e9d3c1832d0
usuario <usuario@10.22.143.10>
2015-12-01 10:27:01 +0000

    kata 6 primer ejercicio última versión (utilizamos la kata 4)

--- src/kata/pkg4v5/Kata4v5.java
+++ src/kata/pkg4v5/Kata4v5.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 public class Kata4v5 {
 
     public static void main(String[] args) throws FileNotFoundException,IOException, ClassNotFoundException, SQLException {
-        String pathname = "C:\\Users\\usuario\\Desktop\\mail.txt";
         ArrayList <Person> people = MailReader.read();
         HistogramBuilder builder = new HistogramBuilder<> (people);
         
@@ -18,7 +17,38 @@ public class Kata4v5 {
                 return person.getMail().split("@")[1];
             }
         });
+        
         HistogramDisplay histoDisplay = new HistogramDisplay(domains);
         histoDisplay.execute();
+        
+        Histogram<Character> letters = builder.build(new Attribute <Person, Character>(){
+            @Override
+            public Character get (Person person){
+                return person.getMail().charAt(0);
+            }
+        });
+        
+        HistogramDisplay histoDisplay2 = new HistogramDisplay(letters);
+        histoDisplay2.execute();
+        
+        Histogram<Float> histoPesos = builder.build(new Attribute <Person, Float>(){
+            @Override
+            public Float get (Person person){
+                return person.getPeso();
+            }
+        });
+        
+        HistogramDisplay histoDisplay3 = new HistogramDisplay(histoPesos);
+        histoDisplay3.execute();
+        
+        Histogram<String> generos = builder.build(new Attribute <Person, String>(){
+            @Override
+            public String get (Person person){
+                return person.getGenero();
+            }
+        });
+        
+        HistogramDisplay histoDisplay4 = new HistogramDisplay(generos);
+        histoDisplay4.execute();
     }
 }
--- src/kata/pkg4v5/MailReader.java
+++ src/kata/pkg4v5/MailReader.java
@@ -1,10 +1,6 @@
 package kata.pkg4v5;
 
-import java.beans.Statement;
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
