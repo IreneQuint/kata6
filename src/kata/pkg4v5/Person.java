e65b993f1aca26f94c1163b588019ee3378ac87f
usuario <usuario@10.22.143.10>
2015-12-01 10:12:56 +0000

    kata 6 primer ejercicio tercera versión (utilizamos la kata 4)

--- src/kata/pkg4v5/Person.java
+++ src/kata/pkg4v5/Person.java
@@ -3,19 +3,36 @@ package kata.pkg4v5;
 public class Person {
 
     private final int id;
+    private final String name;
     private final String mail;
+    private final float peso;
+    private final String genero;
 
-    public Person(int id, String mail) {
+    public Person(int id, String name, String mail, float peso, String genero) {
         this.id = id;
+        this.name = name;
         this.mail = mail;
+        this.peso = peso;
+        this.genero = genero;
     }
 
     public int getId() {
         return id;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public String getMail() {
         return mail;
     }
-    
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
 }
